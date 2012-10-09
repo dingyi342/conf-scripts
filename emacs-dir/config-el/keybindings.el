5211e42d02a4525ceb2c3d43fa1334bc0b85e458
Brandon Betances <syrinx.optimised@gmail.com>
2012-10-09 16:32:57 -0400

    added license keybinding

--- emacs-dir/config-el/keybindings.el
+++ emacs-dir/config-el/keybindings.el
@@ -1,5 +1,21 @@
-;; FILE: emacs-dir/config-el/keybindings.el
-;; AUTHOR: 2012 copyleft syrinx
+;; FILE: /home/syrinx/conf-scripts/emacs-dir/config-el/keybindings.el
+;; AUTHOR: Brandon Betances (Copyleft 2012)
+
+;; LICENSE:
+;;  This program is free software; you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation; either version 2 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program; if not, write to the Free Software
+;;  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 
 ;; COMMENT: global keybindings
 
@@ -40,6 +56,7 @@
 (global-set-key (kbd "C-c c c") 'insert-custom-comment-tag) ;; NOTE: insert a custom comment tag
 (global-set-key (kbd "C-c c s") 'show-custom-comment-tag) ;; NOTE: show custom comment tags
 (global-set-key (kbd "C-c c h") 'insert-custom-header-text) ;; NOTE: insert custom header text for a source code dot file
+(global-set-key (kbd "C-c c l") 'insert-license-lisp) ;; NOTE: insert the GPL license from lisp-license.txt at point
 
 ;; COMMENT: w3m keybindings
 (global-set-key (kbd "C-c C-q") 'w3m-delete-buffer) ;; ;; NOTE: close current w3m buffer
