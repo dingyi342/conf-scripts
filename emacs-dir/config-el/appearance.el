e37057708f4d91ad4f179119848c64fce5d86aac
bbetances <syrinx.optimised@gmail.com>
2012-10-11 16:20:28 -0400

    changed the font size to something more readable

--- emacs-dir/config-el/appearance.el
+++ emacs-dir/config-el/appearance.el
@@ -6,7 +6,8 @@
 ;; (require 'zenburn))
 (require 'color-theme-tangotango)
 (color-theme-tangotango)
-(set-face-attribute 'default nil :height 90) ;; NOTE: change font size
+(set-default-font "-*-terminus-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
+(set-face-attribute 'default nil :height 95) ;; NOTE: change font size
 
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1)) ;; NOTE: hide the menu bar
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1)) ;; NOTE: hide the tool bar
